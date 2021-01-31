02a246806a0cda33184c07e9728a22d0f4497f59
Will Papp <wpapp90@gmail.com>
2021-01-30 18:41:35 -0600

    trying to call api and display results.  Added results container and a few tags on html (non-functional)

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,1 +1,39 @@
 // Weather Dashboard
+
+// Global Variables
+var resultsContainerEl = document.querySelector("#resuts-container");
+var citySearchTerm = document.querySelector('#city-search-term');
+var userFormEl = document.querySelector('#user-form');
+var cityInputEl = document.querySelector('#city');
+var apiKey = "bd3c2a1565ecafc0056ecfa0ed7d9cf7";
+var citySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var citySearched = cityInputEl.value.trim();
+
+    if (citySearched) {
+        getCity(citySearched);
+    }
+}
+
+var getCity = function() {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + apiKey;
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok)
+        response.json().then(function() {
+            resultsContainerEl.textContent = response;
+        })
+        console.log(response)
+    })
+
+        
+}
+
+
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+
+
