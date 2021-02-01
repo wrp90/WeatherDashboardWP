f75e17421d559ff959bae564580801c83de25eab
Will Papp <wpapp90@gmail.com>
2021-02-01 16:40:19 -0600

    heavy html update to better match picture given

--- develop/js/script.js
+++ develop/js/script.js
@@ -5,8 +5,9 @@ var resultsContainerEl = document.querySelector("#resuts-container");
 var citySearchTerm = document.querySelector('#city-search-term');
 var userFormEl = document.querySelector('#user-form');
 var cityInputEl = document.querySelector('#city');
+var searchHistoryContainer = document.querySelector('#search-history-container');
+var searchedCities = [];
 
-// var citySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&appid=" + apiKey;
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -17,6 +18,7 @@ var formSubmitHandler = function(event) {
 
     if (citySearched) {
         getCity(citySearched);
+        displayEl();
     }
 }
 
@@ -24,7 +26,11 @@ var getCity = function() {
     var apiKey = "bd3c2a1565ecafc0056ecfa0ed7d9cf7";
     var city = cityInputEl.value.trim();
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-    localStorage.setItem("city", city)
+    
+    localStorage.setItem("city", city); 
+
+    document.getElementById('search-history-container').textcontent = localStorage.getItem(city);
+
     
     fetch(apiUrl).then(function(response) {
         if (response.ok)
@@ -34,9 +40,14 @@ var getCity = function() {
     })
 }
 
-var displayEl = function(data) {
 
+var displayEl = function() {
+    
 }
+
+// var recentlySearched = function() {
+//     searchedCities = 
+// }
 
 
 
