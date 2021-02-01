b20625357857a664fc57a1217ff9d030924d2d2e
Will Papp <wpapp90@gmail.com>
2021-01-31 21:34:33 -0600

    can now get individual city from fetch request.  City name gets stored to local storage

--- develop/js/script.js
+++ develop/js/script.js
@@ -4,6 +4,7 @@
 var resultsContainerEl = document.querySelector("#resuts-container");
 var citySearchTerm = document.querySelector('#city-search-term');
 var userFormEl = document.querySelector('#user-form');
+var cityInputEl = document.querySelector('#city');
 
 // var citySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&appid=" + apiKey;
 
@@ -21,17 +22,22 @@ var formSubmitHandler = function(event) {
 
 var getCity = function() {
     var apiKey = "bd3c2a1565ecafc0056ecfa0ed7d9cf7";
-    var cityInputEl = document.querySelector('#city');
-    // var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&appid=" + apiKey;
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + apiKey;
-
+    var city = cityInputEl.value.trim();
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    localStorage.setItem("city", city)
+    
     fetch(apiUrl).then(function(response) {
         if (response.ok)
         return response.json();
-    }).then(responseData => {
-        console.log(responseData)
+    }).then(function(data) {
+        console.log(data)
     })
 }
+
+var displayEl = function(data) {
+
+}
+
 
 
 userFormEl.addEventListener('submit', formSubmitHandler);
