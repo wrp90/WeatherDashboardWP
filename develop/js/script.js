651b599c83ee674e8fe1e2882005ab1bf511ea29
Will Papp <wpapp90@gmail.com>
2021-02-01 19:05:37 -0600

    fixed issue where recent searched cites updates dynamically

--- develop/js/script.js
+++ develop/js/script.js
@@ -3,36 +3,50 @@
 // Global Variables
 var citySearchTerm = document.querySelector('#city-search-term');
 var userFormEl = document.querySelector('#user-form');
-// var citiesSearched = [];
+var citiesSearched = [];
 
 
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
     var cityInputEl = document.querySelector('#cityInput');
-
-    var cityInput1 = document.querySelector('#city1');
-    var cityInput2 = document.querySelector('#city2');
-    var cityInput3 = document.querySelector('#city3');
-    var cityInput4 = document.querySelector('#city4');
-    var cityInput5 = document.querySelector('#city5');
+    var city = cityInputEl.value.trim();
     
-    if (cityInput1.textContent === "") {
-        document.getElementById('city1').innerHTML = cityInputEl.value;
-        localStorage.setItem("firstCity", cityInputEl.value);
-    } else if (cityInput2.textContent === "") {
-        document.getElementById('city2').innerHTML = cityInputEl.value;
-        localStorage.setItem("secondCity", cityInputEl.value);
-    } else if (cityInput3.textContent === "") {
-        document.getElementById('city3').innerHTML = cityInputEl.value;
-        localStorage.setItem("thirdCity", cityInputEl.value);
-    } else if (cityInput4.textContent === "") {
-        document.getElementById('city4').innerHTML = cityInputEl.value;
-        localStorage.setItem("fourthCity", cityInputEl.value);
-    } else if (cityInput5.textContent === "") {
-        document.getElementById('city5').innerHTML = cityInputEl.value;
-        localStorage.setItem("fifthCity", cityInputEl.value);
+    if (city) {
+        citiesSearched.unshift(city)
+        recentSearched();
     }
+
+    // var cityInput1 = document.querySelector('#city1');
+    // var cityInput2 = document.querySelector('#city2');
+    // var cityInput3 = document.querySelector('#city3');
+    // var cityInput4 = document.querySelector('#city4');
+    // var cityInput5 = document.querySelector('#city5');
+    
+    // if (cityInput1.textContent === "") {
+    //     document.getElementById('city1').innerHTML = cityInputEl.value;
+    //     localStorage.setItem("firstCity", cityInputEl.value);
+    // } else if (cityInput2.textContent === "") {
+    //     document.getElementById('city2').innerHTML = cityInputEl.value;
+    //     localStorage.setItem("secondCity", cityInputEl.value);
+    // } else if (cityInput3.textContent === "") {
+    //     document.getElementById('city3').innerHTML = cityInputEl.value;
+    //     localStorage.setItem("thirdCity", cityInputEl.value);
+    // } else if (cityInput4.textContent === "") {
+    //     document.getElementById('city4').innerHTML = cityInputEl.value;
+    //     localStorage.setItem("fourthCity", cityInputEl.value);
+    // } else if (cityInput5.textContent === "") {
+    //     document.getElementById('city5').innerHTML = cityInputEl.value;
+    //     localStorage.setItem("fifthCity", cityInputEl.value);
+    // }
+}
+
+var recentSearched = function() {
+    document.getElementById('city1').innerHTML = citiesSearched[0];
+    document.getElementById('city2').innerHTML = citiesSearched[1];
+    document.getElementById('city3').innerHTML = citiesSearched[2];
+    document.getElementById('city4').innerHTML = citiesSearched[3];
+    document.getElementById('city5').innerHTML = citiesSearched[4];
 }
 
 
